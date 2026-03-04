59879a3f4c0407d993dd05443eefe2e9381dca3b
Ogunyebi Ayokanmi <ogunyebiayokanmi@gmail.com>
2026-03-04 10:43:59 +0100

    Update db.ts

--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -1,14 +1,13 @@
-import { PrismaClient } from "../../generated/prisma/client";
-
+import { PrismaClient } from '../../generated/prisma/client';
 
 const prisma = new PrismaClient();
 
 export const connectDb = async () => {
   try {
     await prisma.$connect();
-    console.log("Database connected");
+    console.log('Database connected');
   } catch (error) {
-    console.error("Database connection failed:", error);
+    console.error('Database connection failed:', error);
     process.exit(1);
   }
 };
